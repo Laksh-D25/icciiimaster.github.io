c555f042ade8d8a8ca9b447d72369f794f8450f9
Laksh-D25 <laaaksh25@gmail.com>
2024-09-10 10:26:20 +0530

    Bug fixes - trial for pages

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
     <!-- <landing /> -->
-    <div v-if="active">
-        <navbar />
+    <navbar />
         <div class="z-0">
-            <router-view></router-view>
+            <RouterView />
         </div>
-        <cust_footer />
+    <cust_footer />
+    <!-- <div v-if="active">
+        
     </div>
-    <Landing v-if="!active" />
+    <Landing v-if="!active" /> -->
 </template>
 
 <script setup>
     import navbar from './components/header.vue'
     import cust_footer from './components/footer.vue'
     import Landing from './views/landing.vue';
+    import { RouterView } from 'vue-router';
 
     const active = true
     
